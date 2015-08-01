22d5cc02e45a22b162b0f6f2df497eaad57f1d2c
Caitlin <csettles@users.noreply.github.com>
2015-08-01 13:44:21 -0700

    Formatted for PEP 8 compliance

--- climgur.py
+++ climgur.py
@@ -9,13 +9,16 @@ from auth import get_anon_client, log_in
 from helpers import get_metadata
 
 # file extensions accepted by imgur
-file_extensions = ['.png', '.jpg', '.gif', '.apng', '.bmp', '.jpeg', '.tiff', '.pdf', '.xcf']
+file_extensions = ['.png', '.jpg', '.gif', '.apng',
+                   '.bmp', '.jpeg', '.tiff', '.pdf', '.xcf']
 
 
 class CLUploader:
 
     def __init__(self, args):
         self.args = args
+        self.metadata = dict(title=self.args.title,
+                             description=self.args.description)
         self.initialize()
 
     def initialize(self):
@@ -24,9 +27,6 @@ class CLUploader:
             self.client = get_anon_client()
         else:
             self.client = log_in(get_anon_client())
-
-        self.metadata = dict(title=self.args.title,
-                             description=self.args.description)
 
     def upload_pic(self, path, data, album_id=None):
         anon = self.client.auth is None
@@ -37,13 +37,15 @@ class CLUploader:
         return image['id']  # return image if more data is needed
 
     def upload_album(self):
-        album_data = get_metadata(True) if self.args.metadata else self.metadata
+        album_data = get_metadata(
+            True) if self.args.metadata else self.metadata
         album = self.client.create_album(album_data)
         print('Created album named "{}"'.format(album_data.get('title')))
         album_id = album['id'] if self.client.auth else album['deletehash']
 
         # get all images in the folder with approved file extensions
-        files = [glob(os.path.join(self.args.path, '*'+ext)) for ext in file_extensions]
+        files = [glob(os.path.join(self.args.path, '*' + ext))
+                 for ext in file_extensions]
         files = sum(files, [])  # ugly way to flatten list
 
         for f in files:
@@ -69,10 +71,15 @@ class CLUploader:
 if __name__ == "__main__":
     description = "A command line wrapper for imgur's api"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('path', action='store', help="path to file or folder to upload")
-    parser.add_argument('-t', '--title', action='store', help="title of upload")
-    parser.add_argument('-d', '--description', action='store', help='upload description')
-    parser.add_argument('-u', '--user', action='store_true', help="upload to a user account")
-    parser.add_argument('-m', '--metadata', action='store_true', help="add information to individual images when uploading an album")
+    parser.add_argument(
+        'path', action='store', help="path to file or folder to upload")
+    parser.add_argument(
+        '-t', '--title', action='store', help="title of upload")
+    parser.add_argument(
+        '-d', '--description', action='store', help='upload description')
+    parser.add_argument(
+        '-u', '--user', action='store_true', help="upload to a user account")
+    parser.add_argument('-m', '--metadata', action='store_true',
+                        help="add data to individual images when uploading album")
     args = parser.parse_args()
     CLUploader(args=args).main()
